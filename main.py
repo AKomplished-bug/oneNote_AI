0f58942b2a748360090a0fc66d4ffbb06e3c8e0b
Athul0609 <athulkrishna8781@gmail.com>
2024-04-13 22:23:44 +0530

    loaded keys

--- main.py
+++ main.py
@@ -1,72 +1,54 @@
-import os
-from crewai import Agent, Task, Crew, Process
-from langchain_openai import ChatOpenAI
-from decouple import config
-
+from crewai import Crew
 from textwrap import dedent
 from agents import CustomAgents
 from tasks import CustomTasks
 
-# Install duckduckgo-search for this example:
-# !pip install -U duckduckgo-search
+from dotenv import load_dotenv
+load_dotenv()
 
-from langchain.tools import DuckDuckGoSearchRun
 
-search_tool = DuckDuckGoSearchRun()
-
-os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
-os.environ["OPENAI_ORGANIZATION"] = config("OPENAI_ORGANIZATION_ID")
 
 # This is the main class that you will use to define your custom crew.
 # You can define as many agents and tasks as you want in agents.py and tasks.py
-
-
 class CustomCrew:
-    def __init__(self, var1, var2):
-        self.var1 = var1
-        self.var2 = var2
+    def __init__(self, topic):
+        self.topic = topic
 
     def run(self):
         # Define your custom agents and tasks in agents.py and tasks.py
         agents = CustomAgents()
         tasks = CustomTasks()
 
-        # Define your custom agents and tasks here
-        custom_agent_1 = agents.agent_1_name()
-        custom_agent_2 = agents.agent_2_name()
+        # Define your custom agents
+        note_generation_agent = agents.note_generation_agent()
+        data_agent = agents.data_agent()
+        image_agent = agents.image_agent()
+        structure_agent = agents.Structure_agent()
+        pdf_agent = agents.pdf_agent()
 
-        # Custom tasks include agent name and variables as input
-        custom_task_1 = tasks.task_1_name(
-            custom_agent_1,
-            self.var1,
-            self.var2,
-        )
+        # Define your custom tasks
+        data_task = tasks.data_gathering_task(data_agent, self.topic)
+        image_task = tasks.image_gathering_task(image_agent, self.topic)
+        structuring_task = tasks.structuring_task(structure_agent, data_task, image_task)
+        pdf_task = tasks.pdf_generation_task(pdf_agent, structuring_task)
 
-        custom_task_2 = tasks.task_2_name(
-            custom_agent_2,
-        )
-
-        # Define your custom crew here
+        # Define your custom crew
         crew = Crew(
-            agents=[custom_agent_1, custom_agent_2],
-            tasks=[custom_task_1, custom_task_2],
+            agents=[note_generation_agent, data_agent, image_agent, structure_agent, pdf_agent],
+            tasks=[data_task, image_task, structuring_task, pdf_task],
             verbose=True,
         )
-
         result = crew.kickoff()
         return result
-
 
 # This is the main function that you will use to run your custom crew.
 if __name__ == "__main__":
     print("## Welcome to Crew AI Template")
     print("-------------------------------")
-    var1 = input(dedent("""Enter variable 1: """))
-    var2 = input(dedent("""Enter variable 2: """))
-
-    custom_crew = CustomCrew(var1, var2)
+    topic = input(dedent("""Enter the topic for which you want to generate notes: """))
+    custom_crew = CustomCrew(topic)
     result = custom_crew.run()
     print("\n\n########################")
-    print("## Here is you custom crew run result:")
+    print("## Here is your custom crew run result:")
     print("########################\n")
     print(result)
